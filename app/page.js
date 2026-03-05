0094876f8a804b9ad7f9b0591ff45edc9705cc98
CostaHenry5 <65850100+CostaHenry5@users.noreply.github.com>
2026-03-05 11:43:04 +0300

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,17 @@
+export default function Home() {
+  return (
+    <main style={{padding:"20px",fontFamily:"Arial"}}>
+      <h1>Just Costa</h1>
+      <h3>Data. Digital. Growth.</h3>
+
+      <p>
+        Helping businesses grow with data analytics,
+        digital marketing and branding.
+      </p>
+
+      <a href="mailto:constantinehenry39@gmail.com">
+        Contact Me
+      </a>
+    </main>
+  )
+}
